61313356adb1ae0e3e5bcac6dc8fa4aa860d3dff
DoNalD-A <thdlstjrj@naver.com>
2025-03-13 09:52:12 +0900

    [init] spring-project

---
+++ src/main/java/com/unionclass/spharos_study/SpharosStudyApplication.java
@@ -0,0 +1,13 @@
+package com.unionclass.spharos_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpharosStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpharosStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unionclass/spharos_study/SpharosStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionclass.spharos_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpharosStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
